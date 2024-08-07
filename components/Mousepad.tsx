8cb312cdc7969c0b78d32b19bf2ba359980be63b
Alex Makoviecki <b2alexpilot@gmail.com>
2024-08-06 23:00:16 -0700

    Add mousepad component and other stuff

---
+++ components/Mousepad.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export function MousePad() {
+    return (
+        <View
+            style={{
+                flexDirection: 'column',
+                height: 2200,
+                padding: 10,
+                paddingTop: 30,
+            }}
+        >
+            <View style={{backgroundColor: '#2e3440', flex: 0.3, borderRadius: '20px'}} >
+        </View>
+    )
+}
