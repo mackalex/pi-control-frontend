2b86f0a5fea353bcfb5c5b6773c3a9e9705b8af7
Alex <alexhutman@gmail.com>
2024-08-09 16:18:05 -0400

    Made enum names more wieldy

--- components/Mousepad.tsx
+++ components/Mousepad.tsx
@@ -1,10 +1,10 @@
 import { Colors } from "@/constants/Colors";
 import { Text, View } from "react-native";
-import { getMouseClickEventCommand, BROWSER_MOUSE_BUTTONS, PICTRL_MOUSE_BUTTONS, PICTRL_MOUSE_CLICK } from "@/hooks/protocolBuffer";
+import { getMouseClickEventCommand, BROWSER_MOUSE_BUTTON, PICTRL_MOUSE_BUTTON, PICTRL_MOUSE_CLICK } from "@/hooks/protocolBuffer";
 
-const browser_to_pictrl_clicks = new Map<BROWSER_MOUSE_BUTTONS, PICTRL_MOUSE_BUTTONS>([
-    [BROWSER_MOUSE_BUTTONS.MAIN, PICTRL_MOUSE_BUTTONS.PI_CTRL_MOUSE_LEFT],
-    [BROWSER_MOUSE_BUTTONS.SEC, PICTRL_MOUSE_BUTTONS.PI_CTRL_MOUSE_RIGHT],
+const browser_to_pictrl_clicks = new Map<BROWSER_MOUSE_BUTTON, PICTRL_MOUSE_BUTTON>([
+    [BROWSER_MOUSE_BUTTON.MAIN, PICTRL_MOUSE_BUTTON.LEFT],
+    [BROWSER_MOUSE_BUTTON.SEC, PICTRL_MOUSE_BUTTON.RIGHT],
 ]);
 
 export function MousePad() {
@@ -20,19 +20,19 @@ export function MousePad() {
             <View
             style={{ backgroundColor: Colors.dark.background, flex: 1, borderRadius: '20px' }}
             onMouseDown={(e: MouseEvent) => {
-                let button = e.button as BROWSER_MOUSE_BUTTONS;
+                let button = e.button as BROWSER_MOUSE_BUTTON;
                 if (!(browser_to_pictrl_clicks.has(button))) {
                     return;
                 }
-                const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.PI_CTRL_MOUSE_DOWN);
+                const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.DOWN);
                 console.log(protocolPacket);
             }}
             onMouseUp={(e: MouseEvent) => {
-                let button = e.button as BROWSER_MOUSE_BUTTONS;
+                let button = e.button as BROWSER_MOUSE_BUTTON;
                 if (!(browser_to_pictrl_clicks.has(button))) {
                     return;
                 }
-                const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.PI_CTRL_MOUSE_UP);
+                const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.UP);
                 console.log(protocolPacket);
             }}
             >
