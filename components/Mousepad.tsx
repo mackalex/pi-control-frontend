fe3115f3d5247bedd9fbd3634ea452dc06855cf9
Alex <alexhutman@gmail.com>
2024-08-09 16:07:42 -0400

    Added mouse click support on touchpad

--- components/Mousepad.tsx
+++ components/Mousepad.tsx
@@ -1,5 +1,11 @@
 import { Colors } from "@/constants/Colors";
 import { Text, View } from "react-native";
+import { getMouseClickEventCommand, BROWSER_MOUSE_BUTTONS, PICTRL_MOUSE_BUTTONS, PICTRL_MOUSE_CLICK } from "@/hooks/protocolBuffer";
+
+const browser_to_pictrl_clicks = new Map<BROWSER_MOUSE_BUTTONS, PICTRL_MOUSE_BUTTONS>([
+    [BROWSER_MOUSE_BUTTONS.MAIN, PICTRL_MOUSE_BUTTONS.PI_CTRL_MOUSE_LEFT],
+    [BROWSER_MOUSE_BUTTONS.SEC, PICTRL_MOUSE_BUTTONS.PI_CTRL_MOUSE_RIGHT],
+]);
 
 export function MousePad() {
     return (
@@ -11,7 +17,25 @@ export function MousePad() {
                 paddingTop: 30,
             }}
         >
-            <View style={{ backgroundColor: Colors.dark.background, flex: 1, borderRadius: '20px' }} >
+            <View
+            style={{ backgroundColor: Colors.dark.background, flex: 1, borderRadius: '20px' }}
+            onMouseDown={(e: MouseEvent) => {
+                let button = e.button as BROWSER_MOUSE_BUTTONS;
+                if (!(browser_to_pictrl_clicks.has(button))) {
+                    return;
+                }
+                const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.PI_CTRL_MOUSE_DOWN);
+                console.log(protocolPacket);
+            }}
+            onMouseUp={(e: MouseEvent) => {
+                let button = e.button as BROWSER_MOUSE_BUTTONS;
+                if (!(browser_to_pictrl_clicks.has(button))) {
+                    return;
+                }
+                const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.PI_CTRL_MOUSE_UP);
+                console.log(protocolPacket);
+            }}
+            >
             </View>
         </View>
     )
--- hooks/protocolBuffer.ts
+++ hooks/protocolBuffer.ts
@@ -1,4 +1,4 @@
-const enum COMMANDS {
+const enum PICTRL_COMMANDS {
     PI_CTRL_HEARTBEAT,
     PI_CTRL_MOUSE_MV,
     PI_CTRL_MOUSE_CLICK,
@@ -6,6 +6,31 @@ const enum COMMANDS {
     PI_CTRL_KEYSYM
 }
 
+export const enum PICTRL_MOUSE_BUTTONS {
+    PI_CTRL_MOUSE_LEFT,
+    PI_CTRL_MOUSE_RIGHT,
+}
+
+export const enum PICTRL_MOUSE_CLICK {
+    PI_CTRL_MOUSE_UP,
+    PI_CTRL_MOUSE_DOWN,
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button#value
+export const enum BROWSER_MOUSE_BUTTONS {
+    MAIN,
+    AUX, // Usually wheel or middle
+    SEC, // Usually right click
+    FOURTH,
+    FIFTH
+}
+
 export function getTextEventCommand(charCode: number) {
-    return new Uint8Array([COMMANDS.PI_CTRL_TEXT, 1, charCode])
+    return new Uint8Array([PICTRL_COMMANDS.PI_CTRL_TEXT, 1, charCode])
+}
+
+export function getMouseClickEventCommand(btn: PICTRL_MOUSE_BUTTONS, click: PICTRL_MOUSE_CLICK) {
+    let byte: number = btn << 1;
+    byte |= click;
+    return new Uint8Array([PICTRL_COMMANDS.PI_CTRL_MOUSE_CLICK, 1, byte])
 }
