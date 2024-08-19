08f303ab06c66cab927d0249048356660d2bf0e3
Alex <alexhutman@gmail.com>
2024-08-19 02:24:45 -0400

    Ran Prettier

--- components/Mousepad.tsx
+++ components/Mousepad.tsx
@@ -1,99 +1,110 @@
 import { Colors } from "@/constants/Colors";
 import { View } from "react-native";
 import {
-    RelMouseMove,
-    getMouseClickEventCommand, getMouseMoveEventCommand,
-    BROWSER_MOUSE_BUTTON, PICTRL_MOUSE_BUTTON, PICTRL_MOUSE_CLICK
+  RelMouseMove,
+  getMouseClickEventCommand,
+  getMouseMoveEventCommand,
+  BROWSER_MOUSE_BUTTON,
+  PICTRL_MOUSE_BUTTON,
+  PICTRL_MOUSE_CLICK,
 } from "@/hooks/protocolBuffer";
 import { PiConnectionProps } from "./Connection";
 
-const browser_to_pictrl_clicks = new Map<BROWSER_MOUSE_BUTTON, PICTRL_MOUSE_BUTTON>([
-    [BROWSER_MOUSE_BUTTON.MAIN, PICTRL_MOUSE_BUTTON.LEFT],
-    [BROWSER_MOUSE_BUTTON.SEC, PICTRL_MOUSE_BUTTON.RIGHT],
+const browser_to_pictrl_clicks = new Map<
+  BROWSER_MOUSE_BUTTON,
+  PICTRL_MOUSE_BUTTON
+>([
+  [BROWSER_MOUSE_BUTTON.MAIN, PICTRL_MOUSE_BUTTON.LEFT],
+  [BROWSER_MOUSE_BUTTON.SEC, PICTRL_MOUSE_BUTTON.RIGHT],
 ]);
 
 type RawMouseCoord = {
-    x: number,
-    y: number,
-}
+  x: number;
+  y: number;
+};
 
 type PreviousPointerEvent = {
-    id: number,
-    lastPos: RawMouseCoord
-}
+  id: number;
+  lastPos: RawMouseCoord;
+};
 
-export function MousePad({conn}: PiConnectionProps) {
-    let prev: PreviousPointerEvent = {
-        id: -1,
-        lastPos: {
-            x: -1,
-            y: -1
-        }
-    };
+export function MousePad({ conn }: PiConnectionProps) {
+  let prev: PreviousPointerEvent = {
+    id: -1,
+    lastPos: {
+      x: -1,
+      y: -1,
+    },
+  };
 
-    let delta: RelMouseMove = {
-        x: 0,
-        y: 0
-    };
-    return (
-        <View
-            style={{
-                flexDirection: 'column',
-                flex: 1,
-                padding: 10,
-                paddingTop: 30,
-            }}
-        >
-            <View
-                onContextMenu={(e: PointerEvent) => {
-                    e.preventDefault();
-                }}
-                onPointerDown={(e: PointerEvent) => {
-                    prev.id = e.pointerId;
-                    prev.lastPos.x = e.screenX;
-                    prev.lastPos.y = e.screenY;
-                    let button = e.button as BROWSER_MOUSE_BUTTON;
-                    if (!browser_to_pictrl_clicks.has(button)) {
-                        return;
-                    }
-                    const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.DOWN);
-                    conn.send(protocolPacket);
-                }}
-                onPointerUp={(e: PointerEvent) => {
-                    if (e.pointerId !== prev.id) {
-                        return;
-                    }
+  let delta: RelMouseMove = {
+    x: 0,
+    y: 0,
+  };
+  return (
+    <View
+      style={{
+        flexDirection: "column",
+        flex: 1,
+        padding: 10,
+        paddingTop: 30,
+      }}
+    >
+      <View
+        onContextMenu={(e: PointerEvent) => {
+          e.preventDefault();
+        }}
+        onPointerDown={(e: PointerEvent) => {
+          prev.id = e.pointerId;
+          prev.lastPos.x = e.screenX;
+          prev.lastPos.y = e.screenY;
+          let button = e.button as BROWSER_MOUSE_BUTTON;
+          if (!browser_to_pictrl_clicks.has(button)) {
+            return;
+          }
+          const protocolPacket = getMouseClickEventCommand(
+            browser_to_pictrl_clicks.get(button)!,
+            PICTRL_MOUSE_CLICK.DOWN,
+          );
+          conn.send(protocolPacket);
+        }}
+        onPointerUp={(e: PointerEvent) => {
+          if (e.pointerId !== prev.id) {
+            return;
+          }
 
-                    let button = e.button as BROWSER_MOUSE_BUTTON;
-                    if (!browser_to_pictrl_clicks.has(button)) {
-                        return;
-                    }
-                    const protocolPacket = getMouseClickEventCommand(browser_to_pictrl_clicks.get(button)!, PICTRL_MOUSE_CLICK.UP);
-                    conn.send(protocolPacket);
+          let button = e.button as BROWSER_MOUSE_BUTTON;
+          if (!browser_to_pictrl_clicks.has(button)) {
+            return;
+          }
+          const protocolPacket = getMouseClickEventCommand(
+            browser_to_pictrl_clicks.get(button)!,
+            PICTRL_MOUSE_CLICK.UP,
+          );
+          conn.send(protocolPacket);
 
-                    prev.id = -1;
-                }}
-                onPointerMove={(e: PointerEvent) => {
-                    if (e.pointerId !== prev.id) {
-                        return;
-                    }
-                    delta.x = e.screenX - prev.lastPos.x;
-                    delta.y = e.screenY - prev.lastPos.y;
-                    const protocolPacket = getMouseMoveEventCommand(delta);
-                    conn.send(protocolPacket);
+          prev.id = -1;
+        }}
+        onPointerMove={(e: PointerEvent) => {
+          if (e.pointerId !== prev.id) {
+            return;
+          }
+          delta.x = e.screenX - prev.lastPos.x;
+          delta.y = e.screenY - prev.lastPos.y;
+          const protocolPacket = getMouseMoveEventCommand(delta);
+          conn.send(protocolPacket);
 
-                    prev.lastPos.x = e.screenX;
-                    prev.lastPos.y = e.screenY;
-                }}
-                style={{
-                    backgroundColor: Colors.dark.background,
-                    flex: 1,
-                    borderRadius: '20px',
-                    overscrollBehavior: 'none',
-                    touchAction: 'none',
-                }}
-            >
-            </View>
-        </View>
-    )
+          prev.lastPos.x = e.screenX;
+          prev.lastPos.y = e.screenY;
+        }}
+        style={{
+          backgroundColor: Colors.dark.background,
+          flex: 1,
+          borderRadius: "20px",
+          overscrollBehavior: "none",
+          touchAction: "none",
+        }}
+      ></View>
+    </View>
+  );
 }
--- hooks/protocolBuffer.ts
+++ hooks/protocolBuffer.ts
@@ -1,53 +1,56 @@
 const enum PICTRL_COMMAND {
-    HEARTBEAT,
-    MOUSE_MV,
-    MOUSE_CLICK,
-    TEXT,
-    KEYSYM
+  HEARTBEAT,
+  MOUSE_MV,
+  MOUSE_CLICK,
+  TEXT,
+  KEYSYM,
 }
 
 export const enum PICTRL_MOUSE_BUTTON {
-    LEFT,
-    RIGHT,
+  LEFT,
+  RIGHT,
 }
 
 export const enum PICTRL_MOUSE_CLICK {
-    UP,
-    DOWN,
+  UP,
+  DOWN,
 }
 
 export type RelMouseMove = {
-    x: number,
-    y: number
-}
+  x: number;
+  y: number;
+};
 
 // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button#value
 export const enum BROWSER_MOUSE_BUTTON {
-    MAIN,
-    AUX, // Usually wheel or middle
-    SEC, // Usually right click
-    FOURTH,
-    FIFTH
+  MAIN,
+  AUX, // Usually wheel or middle
+  SEC, // Usually right click
+  FOURTH,
+  FIFTH,
 }
 
 export function getTextEventCommand(charCode: number) {
-    return new Uint8Array([PICTRL_COMMAND.TEXT, 1, charCode])
+  return new Uint8Array([PICTRL_COMMAND.TEXT, 1, charCode]);
 }
 
-export function getMouseClickEventCommand(btn: PICTRL_MOUSE_BUTTON, click: PICTRL_MOUSE_CLICK) {
-    let byte: number = btn << 1;
-    byte |= click;
-    return new Uint8Array([PICTRL_COMMAND.MOUSE_CLICK, 1, byte])
+export function getMouseClickEventCommand(
+  btn: PICTRL_MOUSE_BUTTON,
+  click: PICTRL_MOUSE_CLICK,
+) {
+  let byte: number = btn << 1;
+  byte |= click;
+  return new Uint8Array([PICTRL_COMMAND.MOUSE_CLICK, 1, byte]);
 }
 
 export function getMouseMoveEventCommand(delta: RelMouseMove) {
-    const header = new Uint8Array([PICTRL_COMMAND.MOUSE_MV, 2]);
+  const header = new Uint8Array([PICTRL_COMMAND.MOUSE_MV, 2]);
 
-    const deltaBytesSig = new Int8Array([delta.x, delta.y]);
-    const deltaBytesUnsig = new Uint8Array(deltaBytesSig.buffer);
+  const deltaBytesSig = new Int8Array([delta.x, delta.y]);
+  const deltaBytesUnsig = new Uint8Array(deltaBytesSig.buffer);
 
-    let ret = new Uint8Array(header.length + deltaBytesUnsig.length);
-    ret.set(header);
-    ret.set(deltaBytesUnsig, header.length);
-    return ret;
+  let ret = new Uint8Array(header.length + deltaBytesUnsig.length);
+  ret.set(header);
+  ret.set(deltaBytesUnsig, header.length);
+  return ret;
 }
